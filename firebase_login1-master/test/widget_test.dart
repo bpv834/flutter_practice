c0818843c4403a43e24729cecb82e71361252d7e
ho <yungjoon2@naver.com>
2023-07-15 03:39:08 +0900

    23.10.15 Future 객체 이해

--- firebase_login1-master/test/widget_test.dart
+++ firebase_login1-master/test/widget_test.dart
@@ -9,22 +9,48 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:firebaselogin/main.dart';
+import 'dart:io';
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+import 'dart:io';
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+void main(){
+  showData();
 }
+
+void showData() async{
+  startTask();
+  String
+  String? account = await accessData();
+  fetchData(account!);
+}
+
+void startTask() {
+  String info1 = '요청수행 시작';
+  print(info1);
+}
+
+Future<String?> accessData() async{
+
+  String? account;
+
+  Duration time = Duration(seconds: 3);
+
+  if(time.inSeconds >2) {
+    //sleep(time);
+    await Future.delayed(time, (){
+      account = '8,500만원';
+      print(account);
+    });
+  }else{
+    String info2 = '데이터를 가져왔습니다';
+    print(info2);
+  }
+
+  return account;
+}
+
+void fetchData(String account) {
+  String info3 = '잔액은 $account 입니다';
+  print(info3);
+}
+
