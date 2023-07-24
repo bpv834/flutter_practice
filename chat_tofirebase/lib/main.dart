780c83075704dd5d4a0f1a464a98f0a4d66d3bb3
ho <yungjoon2@naver.com>
2023-07-24 23:35:47 +0900

    23.07.24 채팅앱 유아이 완성후 파이어베이스와 데이터연동

--- chat_tofirebase/lib/main.dart
+++ chat_tofirebase/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:chat_tofirebase/screens/main_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
 
 void main() {
   runApp(const MyApp());
