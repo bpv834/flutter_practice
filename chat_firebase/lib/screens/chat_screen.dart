d5325c751b73d4bba5b1eb2906910886d94009f2
ho <yungjoon2@naver.com>
2023-07-27 03:33:35 +0900

    23.07.27 파이어 베이스 데이터 불러오기, 모달프로그레스 위젯사용

--- chat_firebase/lib/screens/chat_screen.dart
+++ chat_firebase/lib/screens/chat_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -38,7 +39,7 @@ class _ChatScreenState extends State<ChatScreen> {
         title: Text('Chat Screen'),
         actions: [
           IconButton(
-            onPressed: (){
+            onPressed: () {
               //로그아웃
               _authentication.signOut();
               Navigator.pop(context);
@@ -48,8 +49,34 @@ class _ChatScreenState extends State<ChatScreen> {
           )
         ],
       ),
-      body: Center(
-        child: Text('Chat screen'),
+      body: StreamBuilder(
+        //snapshot은 데이터가 바뀔 때마다 새로운 value값을 전달해줍니다.
+        stream: FirebaseFirestore.instance
+            .collection('chats/y8E7Pbe4s5KmvjsrWRi7/message')
+            .snapshots(),
+        builder: (BuildContext context,
+            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final docs = snapshot.data!.docs;
+          return ListView.builder(
+            itemCount: docs.length,
+            itemBuilder: (context, index) {
+              return Container(
+                padding: EdgeInsets.all(8),
+                child: Text(
+                  docs[index]['text'],
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
